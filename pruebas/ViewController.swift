ba390341f6a4f8186025528944e8342a40659aaa
development <development@noes-imac.local>
2021-04-18 18:29:01 -0700

    Initial Commit

---
+++ pruebas/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  pruebas
+//
+//  Created by development on 4/18/21.
+//  Copyright © 2021 development. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
